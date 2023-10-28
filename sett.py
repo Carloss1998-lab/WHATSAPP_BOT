2ed95ba99316a83f1edcd64ced8d0faa042d98ad
caguida <calebcarloss@gmail.com>
2023-10-28 16:03:09 +0200

    first commit

---
+++ sett.py
@@ -0,0 +1,23 @@
+token = 'replybot'
+
+whatsapp_token = 'EAAEpadzUgZCkBO0gZAH4xFmbowmYgxKKqnS19tORv9f43CNlb8d3JzZA4C5cSedB2aZAIUZADOcFuoaTtlqAHETmrzSVAq2a13HojsyEv1Vwy2zUhvCAF1deLhcrz4aX6ZAOIZCfsG3CNVmiJMAvdtO5e4zAM9tY6ZBA6Jm9sqajKw4if8lZASVzhvuZBjbpdhBqykJ7ioLe7NA1pN01bpsPxb91GcwF1r5IutaxLZAZAhgZD'
+whatsapp_url = 'https://graph.facebook.com/v17.0/156524200875526/messages'
+
+
+stickers = {
+    "poyo_feliz": 984778742532668,
+    "perro_traje": 1009219236749949,
+    "perro_triste": 982264672785815,
+    "pedro_pascal_love": 801721017874258,
+    "pelfet": 3127736384038169,
+    "anotado": 24039533498978939,
+    "gato_festejando": 1736736493414401,
+    "okis": 268811655677102,
+    "cachetada": 275511571531644,
+    "gato_juzgando": 107235069063072,
+    "chicorita": 3431648470417135,
+    "gato_triste": 210492141865964,
+    "gato_cansado": 1021308728970759
+}
+
+document_url = "https://www.africau.edu/images/default/sample.pdf"
